74f4f95899416600ed2e4e6ab7f040b0313fdcd7
Daniel Betancourt <dbncourt@gmail.com>
2016-03-06 00:26:17 -0600

    Lecture 18
    Adding functions

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -2,20 +2,35 @@
 #include<string>
 
 using namespace std;
+
+void PrintIntro();
+string GetGuessAndPrintBack();
+
 int main() 
+{
+	PrintIntro();
+	GetGuessAndPrintBack();
+
+	return 0;
+}
+
+void PrintIntro()
 {
 	// introduce the game
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+}
 
+string GetGuessAndPrintBack() 
+{
+	string Guess = "";
 	// get a guess from the player
 	cout << "Enter your guess: ";
-	string Guess = "";
 	getline(cin, Guess);
 
 	// repeat the guess back to them
 	cout << "Your guess was " << Guess << endl;
-
-	return 0;
+	
+	return Guess;
 }
