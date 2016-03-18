68b8ac6cf58c1ed6b4b6d056510d763a3a20cd33
Daniel Betancourt <dbncourt@gmail.com>
2016-03-17 21:09:20 -0600

    Room with door
    The door is open!!

---
+++ BuildingEscape_03/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,38 @@
+// You can do whatever you want with this! :)
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor* Owner = GetOwner();
+	FRotator Rotation = FRotator(0.0f, 0.0f, 0.0f);
+
+	Owner->SetActorRotation(Rotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
