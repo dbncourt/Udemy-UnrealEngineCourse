c17e9b13fbe21271b0a8cbcb5ff7f23a81e135bc
Daniel Betancourt <dbncourt@gmail.com>
2017-02-07 17:37:10 -0600

    211 The Consequences of Inheritance

---
+++ TestingGrounds_05/Source/TestingGrounds_05/PatrollingGuard.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestingGrounds_05.h"
+#include "PatrollingGuard.h"
+
+APatrollingGuard::APatrollingGuard()
+{
+
+}
+
+APatrollingGuard::~APatrollingGuard()
+{
+
+}
---
+++ TestingGrounds_05/Source/TestingGrounds_05/PatrollingGuard.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "PatrollingGuard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_05_API APatrollingGuard : public ATP_ThirdPersonCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	APatrollingGuard();
+	~APatrollingGuard();
+
+private:
+
+	UPROPERTY(EditInstanceOnly, Category = "Patrolling")
+	TArray<AActor*> PatrollingPoints;
+};
